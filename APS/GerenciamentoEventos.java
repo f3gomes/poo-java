be514454dada42d179cacf03e0f5baada185c1b7
Felipe Gomes <fgomesdeluna@gmail.com>
2025-05-01 22:41:50 -0300

    feat: criar classe gerenciamento eventos v1

---
+++ APS/GerenciamentoEventos.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+import java.util.List;
+
+public class GerenciamentoEventos {
+    private List<Show> shows;
+
+    public void adicionarShow(Show show) {
+        shows.add(show);
+    }
+
+    public void listarShows() {
+
+    }
+
+    public void buscarShowPorData(Date data) {
+
+    }
+}
